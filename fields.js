64ee8a8c88c1d1b2e37eda3013d3b09641e649dc
phoussem <houssemeddinegherabi@gmail.com>
2025-05-26 03:42:40 +0100

    Add files via upload

---
+++ fields.js
@@ -0,0 +1,5 @@
+var nameNumber = "extra_fields[custom_field_84jkabeLeFFtMAsO]";
+var nameWilaya = "extra_fields[custom_field_3agUPExC44UfKM1w]";
+var nameCommune = "extra_fields[custom_field_kulVp2EJ92pCpjn3]";
+var stoppdesk = "extra_fields[custom_field_bpkPl7rvjZIKmEaY]";
+var deliveryPrice = "extra_fields[custom_field_fxYCOTcrDrflGANv]";
